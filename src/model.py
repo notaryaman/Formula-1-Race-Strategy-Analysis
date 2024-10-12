ba407a379608e1e2794b95b04cb58ad39db46245
Aryaman Singh <124849392+notaryaman@users.noreply.github.com>
2024-10-11 18:25:08 -0700

    Add files via upload

---
+++ src/model.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 11 03:41:03 2024
+
+@author: singh
+"""
+
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
+import pandas as pd
+
+def train_model(X, y):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    model = RandomForestRegressor(n_estimators=100, random_state=42)
+    model.fit(X_train, y_train)
+    predictions = model.predict(X_test)
+    mse = mean_squared_error(y_test, predictions)
+    print(f'Mean Squared Error: {mse}')
+    return model
---
+++ src/preprocess.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 11 03:40:27 2024
+
+@author: singh
+"""
+
+import pandas as pd
+
+def clean_race_data(df):
+    df = df.dropna(subset=['position', 'laps'])
+    df['position'] = df['position'].astype(int)
+    df['grid'] = df['grid'].astype(int)
+    df['laps'] = df['laps'].astype(int)
+    df['position_change'] = df['grid'] - df['position']
+    df['time'] = pd.to_timedelta(df['time'], errors='coerce').fillna(pd.Timedelta(0))
+    return df
