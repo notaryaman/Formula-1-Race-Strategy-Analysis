afdc9a6da970a78389621e912d51feba820a9c10
Aryaman Singh <124849392+notaryaman@users.noreply.github.com>
2024-10-11 18:26:11 -0700

    Create Analysis.py

---
+++ src/analysis.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 11 03:40:48 2024
+
+@author: singh
+"""
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def plot_position_change(data):
+    sns.boxplot(x='position_change', y='constructor', data=data)
+    plt.title('Position Change by Constructor')
+    plt.show()
+
+def plot_pit_stop_impact(data):
+    sns.scatterplot(x='pit_stop_count', y='position', data=data)
+    plt.title('Pit Stop Count vs Final Position')
+    plt.xlabel('Pit Stop Count')
+    plt.ylabel('Final Position')
+    plt.show()
